560f739d273cc92cea0eb153065ebe56312d4b4c
Skywolf02 <henilchaudhari4422@gmail.com>
2025-08-06 19:57:16 +0530

    last one

---
+++ string.py
@@ -0,0 +1,15 @@
+intro = "My name is Jeff!"
+
+print(intro[0]) 
+print(intro[0:2])
+print(intro[-5:-1])
+
+print(len(intro)) 
+
+print(intro.lower())
+print(intro.upper())
+print(intro.title())
+
+print(intro.split())
+print(intro.split('name')) 
+print(intro.split('!'))
